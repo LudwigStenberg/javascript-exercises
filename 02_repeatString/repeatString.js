6cad7a8fd63ffbf7bbd022139548065a72fbc7b8
LudwigStenberg <lludwigstenberg@gmail.com>
2025-02-18 09:37:12 +0100

    Complete Test 02

--- 02_repeatString/repeatString.js
+++ 02_repeatString/repeatString.js
@@ -1,6 +1,19 @@
-const repeatString = function() {
+const repeatString = function (message, repeatTimes) {
+  let newMessage = message;
 
+  if (repeatTimes === 0) {
+    return "";
+  } else if (repeatTimes < 0) {
+    return "ERROR";
+  }
+
+  for (let i = 0; i < repeatTimes - 1; i++) {
+    newMessage += message;
+  }
+  return newMessage;
 };
+
+repeatString("hey", 3);
 
 // Do not edit below this line
 module.exports = repeatString;
