cb0871dec8d7f8d44f49e76a8d076e5df6f2b076
LudwigStenberg <lludwigstenberg@gmail.com>
2025-02-18 10:21:31 +0100

    Complete Test 03

--- 03_reverseString/reverseString.js
+++ 03_reverseString/reverseString.js
@@ -1,5 +1,9 @@
-const reverseString = function() {
+const reverseString = function (string) {
+  splitString = string.split("");
+  splitString.reverse();
+  let reversedString = splitString.join("");
 
+  return reversedString;
 };
 
 // Do not edit below this line
