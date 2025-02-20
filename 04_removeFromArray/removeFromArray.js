874cdd37edec5fd518ba01625217551b106a3bc7
LudwigStenberg <lludwigstenberg@gmail.com>
2025-02-20 08:58:39 +0100

    Complete  04_removeFromArray

--- 04_removeFromArray/removeFromArray.js
+++ 04_removeFromArray/removeFromArray.js
@@ -1,4 +1,13 @@
-const removeFromArray = function() {
+// const removeFromArray = function (array, valueToRemove) {
+//   const newArray = array.filter((element) => element !== valueToRemove);
+//   return newArray;
+// };
+
+const removeFromArray = function (array, ...valuesToRemove) {
+  const filteredArray = array.filter(
+    (element) => !valuesToRemove.includes(element)
+  );
+  return filteredArray;
 };
 
 // Do not edit below this line
