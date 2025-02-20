0f5d8d8c3053ec2a5c70ee1e2da4e71b665c78c9
LudwigStenberg <lludwigstenberg@gmail.com>
2025-02-20 09:38:55 +0100

    Complete  05_sumAll

--- 05_sumAll/sumAll.js
+++ 05_sumAll/sumAll.js
@@ -1,5 +1,32 @@
-const sumAll = function() {
+const sumAll = function (firstNum, secondNum) {
+  let startNum, endNum;
 
+  if (
+    firstNum < 0 ||
+    secondNum < 0 ||
+    Number.isNaN(firstNum) ||
+    Number.isNaN(secondNum) ||
+    !Number.isInteger(firstNum) ||
+    !Number.isInteger(secondNum)
+  ) {
+    return "ERROR";
+  }
+
+  if (firstNum < secondNum) {
+    startNum = firstNum;
+    endNum = secondNum;
+  } else {
+    startNum = secondNum;
+    endNum = firstNum;
+  }
+
+  sum = 0;
+
+  for (let i = startNum; i <= endNum; i++) {
+    sum += i;
+  }
+
+  return sum;
 };
 
 // Do not edit below this line
