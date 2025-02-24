bd7565a6b3ca368733e993940dce9f04e5cc5971
LudwigStenberg <lludwigstenberg@gmail.com>
2025-02-24 08:52:56 +0100

    Complete  06_leapYears

--- 06_leapYears/leapYears.js
+++ 06_leapYears/leapYears.js
@@ -1,5 +1,17 @@
-const leapYears = function() {
+const leapYears = function (year) {
+  let isLeapYear = false;
 
+  if (Number.isInteger(year / 4) && !Number.isInteger(year / 100)) {
+    isLeapYear = true;
+  } else if (
+    Number.isInteger(year / 4) &&
+    Number.isInteger(year / 100) &&
+    Number.isInteger(year / 400)
+  ) {
+    isLeapYear = true;
+  }
+
+  return isLeapYear;
 };
 
 // Do not edit below this line
