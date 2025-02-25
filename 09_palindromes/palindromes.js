db3f3b087ea2c60bf0ec08d6ff8666976428968c
LudwigStenberg <lludwigstenberg@gmail.com>
2025-02-25 09:55:54 +0100

    Complete 09_palindromes

--- 09_palindromes/palindromes.js
+++ 09_palindromes/palindromes.js
@@ -1,6 +1,18 @@
-const palindromes = function () {
+const palindromes = function (text) {
+  // Removes special characters and converts to lower case.
+  const cleanText = text.toLowerCase().replace(/[^a-z0-9]/g, "");
 
+  let length = Math.floor(cleanText.length / 2);
+
+  for (let i = 0; i < length; i++) {
+    if (cleanText[i] !== cleanText[cleanText.length - i - 1]) {
+      return false;
+    }
+  }
+
+  return true;
 };
 
+console.log(palindromes("racecar!"));
 // Do not edit below this line
 module.exports = palindromes;
